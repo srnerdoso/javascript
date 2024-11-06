080f92dd2a43f762e7e9054d18d6cfe2f00dec70
Valdenor <vfval09@gmail.com>
2024-11-06 20:16:21 -0300

    Update exercicio013

--- exercícios/ex013/script.js
+++ exercícios/ex013/script.js
@@ -1,45 +1,109 @@
-const players = ["x", "o"];
+const options = ["x", "o"];
+const players = []
 
 let buttonIndex = 0;
-function createButtons() {
+function initializeGameScene() {
   const div = document.createElement("div");
   div.id = "buttons-container";
   document.querySelector("main").appendChild(div);
 
+  const playersContainer = document.getElementById("players-container")
+  const p1Name = document.getElementById("p1Name").value
+  const p2Name = document.getElementById("p2Name").value
+
+  players.push(p1Name, p2Name)
+
+  const h2 = document.createElement("h2")
+  h2.innerText = "Jogadores:"
+  playersContainer.appendChild(h2)
+
+  const p1 = document.createElement("p")
+  p1.className = "player-select"
+  p1.innerText = p1Name
+  const p2 = document.createElement("p")
+  p2.className = "player-select"
+  p2.innerText = p2Name
+
+  playersContainer.append(p1, p2)
+  playersContainer.id = "playersContainer"
+
   for (let i = 0; i < 9; i++) {
     const button = document.createElement("button");
-    button.id = "square-" + buttonIndex;
+    // button.id = "square-" + buttonIndex;
     button.className = "squares";
 
     buttonIndex++;
 
     div.appendChild(button);
   }
+
+  document.getElementById("player1").remove()
+  document.getElementById("player2").remove()
 }
 
 let playerIndex = 0;
-function getCurrentPlayer() {
-  return players[playerIndex];
+
+function getCurrentPLayer() {
+  return players[playerIndex]
+}
+
+function getCurrentOption() {
+  return options[playerIndex];
 }
 
 function switchPlayer() {
   playerIndex = (playerIndex + 1) % players.length;
+
+  const playersList = document.querySelectorAll("p")
+  togglePlayer(playersList[0], playersList[1])
+}
+
+function togglePlayer(player1, player2) {
+  if (playerIndex === 0) {
+    player1.className = "selected-player"
+    player2.className = "player-select"
+  } else {
+    player1.className = "player-select"
+    player2.className = "selected-player"
+  }
 }
 
 function getPlayerMove(buttonSquare) {
+  const playersList = document.querySelectorAll("p")
+  togglePlayer(playersList[0], playersList[1])
+  
   let mouseoverEvent = function () {
-    buttonSquare.className = "squares-" + getCurrentPlayer() + "-hover";
+    buttonSquare.className = "squares-" + getCurrentOption() + "-hover";
   };
 
   buttonSquare.addEventListener("mouseover", mouseoverEvent);
 
+  let win = false
   buttonSquare.addEventListener("click", function () {
     buttonSquare.removeEventListener("mouseover", mouseoverEvent);
 
     buttonSquare.className = "squares-clicked";
-    buttonSquare.innerText = getCurrentPlayer().toUpperCase();
+    buttonSquare.innerText = getCurrentOption().toUpperCase();
 
     buttonSquare.disabled = true;
+
+    for (let i = 0; i < 8; i++) {
+      if (checkWin(i)) {
+        win = checkWin(i)
+        break
+      }
+    }
+
+    if (win) {
+      for (let i = 0; i < 9; i++) {
+        const buttons = document.getElementById("buttons-container").children
+        buttons[i].removeEventListener("mouseover", mouseoverEvent)
+        // buttons[i].className = "squares-clicked"   Sem funcionar por enquanto, tentarei resolver amanhã
+        buttons[i].disabled = true
+      }
+
+      console.log("FUNCIONOU!!!!!!!")
+    }
 
     switchPlayer();
   });
@@ -48,7 +112,7 @@ function getPlayerMove(buttonSquare) {
 }
 
 function play() {
-  createButtons();
+  initializeGameScene();
 
   document.querySelectorAll(".squares").forEach(function (square) {
     getPlayerMove(square);
@@ -64,7 +128,7 @@ function resetGame() {
   play();
 }
 
-function checkWin() {
+function checkWin(index) {
   const wins = [
     { position1: 0, position2: 1, position3: 2 },
     { position1: 3, position2: 4, position3: 5 },
@@ -78,25 +142,15 @@ function checkWin() {
     { position1: 2, position2: 4, position3: 6 },
   ];
 
-  const squares = document.querySelectorAll(".squares");
+  const squares = document.getElementById("buttons-container").children
 
-  const verifyList = [];
+  let optionsIndex0 = options[0].toUpperCase()
 
-  let pos1;
-  let pos2;
-  let pos3;
-  for (let i = 0; i < 8; i++) {
-    pos1 =
-      squares[wins[i].position1].textContent === getCurrentPlayer().toUpperCase();
-    pos2 =
-      squares[wins[i].position2].textContent === getCurrentPlayer().toUpperCase();
-    pos3 =
-      squares[wins[i].position3].textContent === getCurrentPlayer().toUpperCase();
+  let position1 = squares[wins[index].position1].innerText == optionsIndex0
+  let position2 = squares[wins[index].position2].innerText == optionsIndex0
+  let position3 = squares[wins[index].position3].innerText == optionsIndex0
 
-    if (pos1 && pos2 && pos3) {
-      verifyList.push(pos1, pos2, pos3);
-    }
-  }
+  return position1 + position2 + position3 === 3
 }
 
 function checkGameOutcome() {}
