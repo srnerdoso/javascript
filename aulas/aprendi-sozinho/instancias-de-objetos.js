e35cdff560ede49fc7cf7f86fb32cb33d6547bc8
Valdenor <vfval09@gmail.com>
2025-02-12 15:16:15 -0300

    New skill
    - Instantiating objects

---
+++ aulas/aprendi-sozinho/instancias-de-objetos.js
@@ -0,0 +1,13 @@
+const instace = {};
+instace["hello-world"] = []
+instace["ola-mundo"] = []
+
+console.log(instace)
+
+instace["hello-world"].push("hello")
+instace["hello-world"].push("world")
+
+instace["ola-mundo"].push("ola")
+instace["ola-mundo"].push("mundo")
+
+console.log(instace)
